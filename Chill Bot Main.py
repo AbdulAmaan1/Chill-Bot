78a6401d86cde62c8ea14c23f683dcbbcbdcd2e4
Amaan <88821985+AbdulAmaan1@users.noreply.github.com>
2021-10-15 22:19:41 +0530

    new function made

--- Chill Bot Main.py
+++ Chill Bot Main.py
@@ -23,14 +23,18 @@ async def on_message(message):
 # ping pong
 @client.command()
 async def ping(ctx):
+  
+#calculates stuff
+@client.command()
+async def calculate(ctx):
     j=ctx.message.content
     j=j[10:].lower()
     print(type(j))
-    trig_flags=['sin','tan','cos','cosec','sec','cot']
+    trig_flags=['sin','tan','cos','cosec','sec','cot']#flass to search so that it can evaluate trig functions seperately
     other_mathflags=['^','']
     if j in trig_flags:
         flags=[]
-        for i in trig_flags:
+        for i in trig_flags:#chekcing which functions are there
             r=j.find(i)
             if r==-1:
                 flags.append(i)
@@ -39,7 +43,6 @@ async def ping(ctx):
     p=eval(str(j))
     print(p)
     await ctx.send(p)
-
 
 # running the bot
 client.run("ODk4NTcwMzU0OTA0MTU0MTYy.YWmI2w.NZtpa5f83RJrJouULNytn9bynEU")
