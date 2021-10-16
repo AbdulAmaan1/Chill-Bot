8f8c8840c16cf43f08c3ba64dd77b5f0dad202c7
Aravind2v <89016600+Aravind2v@users.noreply.github.com>
2021-10-16 09:31:18 +0530

    8 ball command
    made the list as a column , corrected typo, added more phrases

--- Chill Bot Main.py
+++ Chill Bot Main.py
@@ -24,13 +24,41 @@ async def on_message(message):
 @client.command()
 async def ping(ctx):
     await ctx.send("pong!")
+
 #8ball
-@client.command(aliases=['8ball','ball','8b'])
+@client.command(aliases=['8ball'])
 async def eightball (x):
 
-    response=["it's woth it","it's bad","you could have don it better","you are the worst testing me ","it is all well","it is good "]
+    response=[
+    "it's woth it",
+    "it's bad",
+    "you could have done it better",
+    "you are the worst testing me ",
+    "it is all well",
+    "it is good ",
+    'As I see it',
+    ' yes Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'Don’t count on it',
+    'It is certain',
+    'It is decidedly so',
+    'Most likely',
+    'My reply is no',
+    'My sources say no',
+    'Outlook good',
+    'Outlook not so good',
+    'Signs point to yes',
+    'Very doubtful',
+    'Without a doubt',
+    ' Yes',
+    ' definitely',
+    'You may rely on it ' 
+     ]
 
     await x.send(choice(response))
 
 # running the bot
-client.run("token")
+client.run("")
