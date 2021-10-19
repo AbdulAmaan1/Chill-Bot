e0ed4b599eac1895da79db98d7c6818ae086c916
Amaan <88821985+AbdulAmaan1@users.noreply.github.com>
2021-10-19 17:58:46 +0530

    bot done

--- Chill Bot Main.py
+++ Chill Bot Main.py
@@ -104,6 +104,7 @@ async def quote(ctx):
 
 
 # user defined function for calculator feature    
+#calculator command
 @client.command()
 async def cal(ctx):
     heading,con,embed='','',''
@@ -114,19 +115,44 @@ async def cal(ctx):
         trig_flags=['sin','tan','cosec','cos','sec','cot']
         other_flags=['^','log','ln','^b']
         flags=trig_flags+other_flags
-        for i in flags:# checks which trig functions are there and then evauluates them
-            r=j.find(i)
+        #continued_flag=None
+        inner=False
+        trig_loop=False
+        #tim=0
+        jojo=0
+        br=False
+        global c
+        c=0
+        while jojo<len(flags):# checks which trig functions are there and then evauluates them
+            flag=flags[jojo]
+            r=j.find(flag)
             if r!=-1:
-                t=j.split(i)[1:]# removes and seperates all trig functions from string and seperates their values 
+                t=j.split(flag)[1:]# removes and seperates all trig functions from string and seperates their values 
                 #string_to_replace,res=extract(t,0,i)#extracts values
-                
+                if '))'in j or c!=0:
+                    index=j.rfind('(')
+                    if j[index-2:index]=='ln':
+                        flag=j[index-2:index]
+                    else:
+                        flag=j[index-3:index]
+                    if flag=='sec':
+                        if j[index-5:index-3]=='co':
+                            flag='cosec'
+                        else:
+                            flag='sec'
+                    other_index=j.rfind(')')
+                    t=[j[index:other_index+1]]
+                    a=j.rfind(')')
+                    if trig_loop==False:
+                        c=j.count(')',index,a+1)+2
+                    trig_loop=True
+                        
+
                 for q in range(len(t)):
-                    print(j)
                     res,base,count='','',0
                     comma_detector=False
                     while True:
                         if count<len(t[q]):
-                            print(res,'base',base,comma_detector)
                             if t[q][count]==',' or comma_detector==True:
                                 if t[q][count]!=')':    
                                     base+=t[q][count]
@@ -139,6 +165,8 @@ async def cal(ctx):
                                 if t[q][count]!=')':
                                     res+=t[q][count]
                                     count+=1    
+                                    if t[q][count] in ['+','/','-','*']:
+                                        br=True
                                 else:
                                     res+=t[q][count]
                                     break
@@ -146,59 +174,56 @@ async def cal(ctx):
                             break
                     
                     if comma_detector==False:
-                        string_to_replace=i+res
+                        string_to_replace=flag+res
                     else:
-                        string_to_replace=i+res+base
-                    print(string_to_replace)
-                    if i in trig_flags:
+                        string_to_replace=flag+res+base
+                    if flag in trig_flags:
+                        if br==True and 'deg'in res:
+                            res='('+str(eval(res[1:-4]))+res[-4:]
+                        else:
+                            res='('+str(eval(res))+')'
                         num=res.lstrip('(').rstrip(')')#edits it so that it can be accepted by the float method
                         cosec_check=False#checks if its a cosec function, had to do this on top of the if condition to prevent error
-                        if 'rad' in num:#checks if the value inside trig functions is in radians else converts them to radians
-                            u=num.find('rad')
+                        if 'deg' in num:#checks if the value inside trig functions is in radians else converts them to radians
+                            u=num.find('deg')
                             num=float(num[:u])
+                            num=math.radians(num)
                         else:
-                            num=math.radians(float(num))
-                        if i=='sin':
+                            num=float(num)
+                        if flag=='sin':
                             answer=math.sin(num)
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)
-                        elif i=='cos':
+                        elif flag=='cos':
                             answer=math.cos(num)
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)
-                        elif i=='tan':
+                        elif flag=='tan':
                             answer=math.tan(num)
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)
-                        elif i=='sec':
+                        elif flag=='sec':
                             answer=math.cos(num)
                             answer=1/answer
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)
-                        elif i=='cot':
+                        elif flag=='cot':
                             answer=math.tan(num)
                             answer=1/answer
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)
-                        elif i=='cosec':
+                        elif flag=='cosec':
                             answer=math.sin(num)
                             answer=1/answer
-                            answer='('+str(answer)+')'
-                            j=j.replace(string_to_replace,answer)#replacing the trig function in the expression string with the value of the function, so that the eval function can evaulate it at the end
-                            print(j)
+                        if trig_loop==True:
+                            answer=str(answer)
+                        else:
+                            answer='('+str(answer)+')'   
+                        j=j.replace(string_to_replace,answer)
+                        #replacing the trig function in the expression string with the value of the function, so that the eval function can evaulate it at the end
                     else:
-                        if i=='^':
+                        if br==True:
+                            res='('+str(eval(res))+')'
+                        if flag=='^':
                             j=j.replace('^','**')#replaces ^ symbol for ** as in python we represent power with '**' symbol and '^' is binary or 
-                        elif i=='^b':
+                        elif flag=='^b':
                             j=j.replace('^b','^')#replaces ^b with ^ symbol so that the eval function can evaluate it as binary or
-                        elif i=='ln':
+                        elif flag=='ln':
                             num=float(res.lstrip('(').rstrip(')'))
-                            print(num)
                             answer=str(math.log(num))
                             j=j.replace(string_to_replace,answer)#same replacing thing
-                        elif i=='log':#here it also takes input in form log(value,base) so thats why the if else with the comma detector
+                        elif flag=='log':#here it also takes input in form log(value,base) so thats why the if else with the comma detector
                             num=float(res.lstrip('(').rstrip(')'))
-                            print(num,string_to_replace,base)
                             if comma_detector==True:
                                 base=float(base.lstrip(',').rstrip(')'))
                                 #comma_detector=False
@@ -206,9 +231,11 @@ async def cal(ctx):
                                 base=10
                             answer=str(math.log(num,base))
                             j=j.replace(string_to_replace,answer)
-
+            if c==0:
+                jojo+=1
+            if c!=0 :
+                c-=1
         p=eval(j)
-        print(p)
         con=str(round(p,3))
         heading='Answer'
         embed=discord.Embed(title=heading,content=con,colour=discord.Colour.random())
@@ -245,7 +272,6 @@ async def hehe(ctx):#hehe boi
 async def lol(ctx):#LOL NOOB
     em=discord.Embed(title='LOL',colour=discord.Colour.random())
     em.set_image(url="https://c.tenor.com/x5MCO3i9ArUAAAAd/el-risitas-risitas.gif")
-    await ctx.send(embed=em) 
-    
+    await ctx.send(embed=em)
 # running the bot
-client.run("ODk3Mzk4NTYzODA0NDQ2ODAx.YWVFig.V0cNd_RvlUuTPtwvWPoDA-InNHc")
+client.run("ODk3NDAwMjY1NTIxNjUxNzEy.YWVHIA.3bcENUBJMkpCQI3J-zIc2vaZoK4")
